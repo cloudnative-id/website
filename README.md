6104828672e13435ff9c791fd257545ac212c5ea
Eufrat Tsaqib <eutsat@gmail.com>
2020-01-18 18:19:29 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# cloud-native-indonesia-website
+
+> Website to display cloud native indonesia stats
+
+## Build Setup
+
+### Install yarn
+Follow this [installation guide](https://yarnpkg.com/lang/en/docs/install) to install yarn.
+
+### Install Node.js > v10
+This repo uses dependencies which requires Node.js version 10 or above.
+
+Follow this [installation guide](https://nodejs.org/en/download/package-manager/) to install node.js v10.x.
+
+### Build the website
+This website is built using the Vue.js Framework.
+
+``` bash
+# install dependencies
+$ yarn
+
+# serve with hot reload at localhost:3000
+$ npm run dev
+
+# build for production and launch server
+$ npm run build
+$ npm run start
+
+# generate static project
+$ npm run generate
+```
+
+For detailed explanation on how things work, checkout [Nuxt.js docs](https://nuxtjs.org).
---
+++ nuxt.config.js
@@ -0,0 +1,81 @@
+export default {
+  mode: 'universal',
+  /*
+  ** Headers of the page
+  */
+  head: {
+    titleTemplate: 'Cloud Native Indonesia',
+    title: 'Cloud Native Indonesia',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+,
+      {
+        rel: 'stylesheet',
+        href:
+          'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons'
+      },
+      {
+        rel: 'stylesheet',
+        href:
+          'https://fonts.googleapis.com/css?family=Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&display=swap'
+      },
+    ]
+  },
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#fff' },
+  /*
+  ** Global CSS
+  */
+  css: [
+    '~/assets/style/app.scss',
+  ],
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [
+    '@/plugins/helpers',
+  ],
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+    "@nuxtjs/vuetify",
+    '@nuxtjs/pwa',
+    '@nuxtjs/apollo',
+  ],
+
+  vuetify: {
+    customVariables: ["~/assets/variables.scss"],
+    theme: {
+    }
+  },
+
+  apollo: {
+    // required
+    clientConfigs: {
+      default: {
+        httpEndpoint: 'https://stats-api.cloudnativecommunity.id/query',
+        // httpEndpoint: 'http://localhost:8080/query',
+        // Enable Automatic Query persisting with Apollo Engine
+        persisting: true,
+      }
+    }
+  },
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** You can extend webpack config here
+    */
+    extend(config, ctx) {
+    }
+  }
+}
